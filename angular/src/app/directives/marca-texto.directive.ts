a3a0c2abcaca93525901a15a4b14742d8c44688e
cleysoncassio <cleysoncassio@gmail.com>
2022-07-30 13:11:58 -0300

    esclarecimentos

--- angular/src/app/directives/marca-texto.directive.ts
+++ angular/src/app/directives/marca-texto.directive.ts
@@ -21,3 +21,12 @@ export class MarcaTextoDirective implements OnInit {
 
    }
 
+//diretivas são meios para manipular o DOM ou estender as funcionalidades do elemento
+//diretivas costumizadas altera um atributo ou estrutura
+//Ngif é uma diretiva estrutural
+//Ngclass é diretiva de atributo
+//NgStyle atualiza estilos para o elemento HTML que a contém
+//Ngclass adiciona ou remove classes CSS em um elemento de acordo com determinada regra
+//*Ngif é uma diretiva estrutural que possui uma condição booleanda que quando verdadeira, o angular renderiza o elemento
+// e quando falso não renderiza
+//*Ngswith exibe um elemento de um possíve conjunto de elementos com base em alguma condição, ele usa diretivas NgSwitch e ngSwitchDefault
