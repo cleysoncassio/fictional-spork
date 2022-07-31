1602af7580c16213fb022a28e4f5152ce3274509
cleysoncassio <cleysoncassio@gmail.com>
2022-07-31 00:19:37 -0300

    Update list.service.ts

--- angular/src/app/shared/services/list.service.ts
+++ angular/src/app/shared/services/list.service.ts
@@ -21,3 +21,13 @@ export class ListService {
 
   }
 }
+
+
+//Provideres informa aos componentes que o serviço está disponível para uso
+//ProvideIn define como e quem pode injetar esse serviço como dependência, normalmente o root diz que qualquer
+//componente pode recebê-lo
+// Injeção de dependência = Um padrão de design no qual uma classe solicita dependências de fontes externas em vez de criá-las
+//Integração com APIS SÃO GERALMENTE ATRIBUIDAS A SERVIÇOS.
+//Injeção de dependência é feita no constructor da classe.Constructor().
+//serviços são unidades centrais para algumas funções comuns emm todo o aplicativo e possui@injectable quando precisa ser usado
+//
